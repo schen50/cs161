73ffb32f4de5e547e8cdf3787ff650cc27695860
Aaron Levett <alevett@stanford.edu>
2018-03-08 21:04:44 -0800

    Structure for Fast

--- CLCSFast.py
+++ CLCSFast.py
@@ -4,10 +4,24 @@ import numpy as np
 arr = np.zeros((2048, 2048), dtype=int)
 
 
+
+def find_shortest_path(A, B, p, lower, upper): #p is a global variable for now
+    if upper - lower <= 1:
+        return
+    mid = float(lower + upper)/2
+    paths[mid] = single_shortest_path(A, B, mid, paths[lower], paths[mid])
+    find_shortest_path(A, B, lower, mid)
+    find_shortest_path(A, B, mid, upper)
+
+
+def single_shortest_path(A, B, mid, low_path, mid_path):
+    return 
+
+
+
 def LCS(A, B):
     m = len(A)
     n = len(B)
-
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if A[i - 1] == B[j - 1]:
@@ -24,8 +38,11 @@ def main():
 
     for l in sys.stdin:
         A, B = l.split()
-        print
-        LCS(A, B)
+        m = len(A)
+        p = [[] for i in range(0, m)]
+        A = A + A
+        paths[0] = single_shortest_path(A, B, p, [], [])
+        find_shortest_path(A, B, p, 0, m)
     return
 
 
