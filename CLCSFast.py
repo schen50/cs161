20e109d4a5dc65ff5c46f5e1b92defca86841d56
Aaron Levett <alevett@stanford.edu>
2018-03-10 00:30:48 -0800

    Getting path in backtrace for simple case is working

--- CLCSFast.py
+++ CLCSFast.py
@@ -3,15 +3,13 @@ import numpy as np
 
 arr = np.zeros((2048, 2048), dtype=int)
 
-# min_for_row = {}
-# max_for_row = {}
-
-#PATH  = [(L_Min0, U_Max0), (L_Min1, U_Max1), (L_Min2, U_Max2), ()]
 
 def find_shortest_path(A, B, p, lower, upper): #p
     if upper - lower <= 1:
         return
     mid = float(lower + upper)/2
+    print p[lower]
+    print p[upper]
     p[mid] = single_shortest_path(A, B, mid, p[lower], p[upper])
     find_shortest_path(A, B, lower, mid)
     find_shortest_path(A, B, mid, upper)
@@ -19,27 +17,24 @@ def find_shortest_path(A, B, p, lower, upper): #p
 def single_shortest_path(A, B, mid, low_path, upper_path):
     return
 
-#[(Min for lower bound, max for upper bound)]
+#[(Min for lower bound INCLUSIVE, max for upper bound INCLUSIVE)]
 
 def backtrace_full_LCS(A, B): #Maybe only use for path 0 case? Then write more elaborate method for boundary checking?
     m = len(A)
     n = len(B)
     lcs_length = 0
-    path = [(0,0) for i in range(0, m)]
+    path = [[0,n] for i in range(0, m+1)] #extra row
     while m > 0 and n > 0:
         if A[m-1] == B[n-1]:
             path[m-1][1] = n-1 #new row in upper bound case
             path[m][0] = n     #old row in lower bound case
-
             m = m - 1
             n = n - 1
             lcs_length += 1
-
-
         elif arr[m-1][n] < arr[m][n-1]:
             #BACKTRACE BY MOVING LEFT ON GRAPH. No updates necessary in upper bound case. 
                                                 #min for row updated in lower bound case
-            path[m][0] = n-1
+            path[m][0] = n-1 #current row in lower bound case
             n -= 1
         else:
             #BACKTRACE BY MOVING UP ON GRAPH. Update max for new row in upper bound case. 
@@ -47,6 +42,7 @@ def backtrace_full_LCS(A, B): #Maybe only use for path 0 case? Then write more e
             path[m-1][1] = n #new row in upper bound case
             path[m][0] = n #old row in lower bound case
             m -= 1
+    return path
 
 def LCS(A, B):
     m = len(A)
@@ -57,7 +53,6 @@ def LCS(A, B):
                 arr[i][j] = arr[i - 1][j - 1] + 1
             else:
                 arr[i][j] = max(arr[i - 1][j], arr[i][j - 1])
-
     return arr[m][n]
 
 
@@ -68,10 +63,10 @@ def main():
     for l in sys.stdin:
         A, B = l.split()
         m = len(A)
-        p = [[] for i in range(0, m)]
+        p = [[] for i in range(0, m+1)]
         #Update array for initialization, don't need to return anything from LCS
         LCS(A, B)
-        p[0] = backtrace_full_LCS(A, B, p)
+        p[0] = backtrace_full_LCS(A, B)
         p[m] = p[0]
         A = A + A
         find_shortest_path(A, B, p, 0, m)
