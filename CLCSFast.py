90d799a7831dcdff0e5029649c6515cf56867d93
Aaron Levett <alevett@stanford.edu>
2018-03-11 10:52:56 -0700

    Way better than b4. Still need to work on backtracking bonunds

--- CLCSFast.py
+++ CLCSFast.py
@@ -1,7 +1,8 @@
 import sys
 import numpy as np
+import pdb
 
-arr = np.zeros((8, 8), dtype=int)
+arr = np.zeros((2048, 2048), dtype=int)
 max_lcs_length = 0
 #
 #
@@ -29,7 +30,7 @@ def find_shortest_path(A, B, p, lower, upper):
 def single_shortest_path(A, B, mid, top_path, bottom_path, top_row, bottom_row):
     m = len(A)/2
     n = len(B)
-
+    #print A
     #
     #TODO: FIGURE OUT WHICH CLEARING IS NECESSARY (or if both are given odd/even case...)
     #
@@ -45,20 +46,43 @@ def single_shortest_path(A, B, mid, top_path, bottom_path, top_row, bottom_row):
     #print "BEFORE: ", arr
 
 
+    # def LCS(A, B):
+    # m = len(A)
+    # n = len(B)
+    # for i in range(1, m + 1):
+    #     for j in range(1, n + 1):
+    #         if A[i - 1] == B[j - 1]:
+    #             arr[i][j] = arr[i - 1][j - 1] + 1
+    #         else:
+    #             arr[i][j] = max(arr[i - 1][j], arr[i][j - 1])
+    # return arr[m][n]
+
+
+ 
     for i in range(mid+1, m+mid+1):
-        for j in range(bottom_path[i][0], top_path[i][1]+1): 
-            if A[i - 1] == B[j - 1] and (bottom_path[i-1][0] <= j - 1 <= top_path[i-1][1]):    
+        for j in range(bottom_path[i][0], top_path[i][1]+1):
+            if A[i - 1] == B[j - 1] and (bottom_path[i-1][0] <= j - 1 <= top_path[i-1][1]):   #possible diagnoal and in bounds
                 arr[i][j] = arr[i - 1][j - 1] + 1
             else:
-                #TODO: ADD BOUND CHECKING BASED ON LOWER AND UPPER PATHS
-                if (bottom_path[i-1][0] <= j <= top_path[i-1][1]):
-                    if (bottom_path[i][0] <= j - 1): #not sure if we actually need to check this
-                        #print "here"
-                        arr[i][j] = max(arr[i - 1][j], arr[i][j - 1])
-                    # else:
-                    #      arr[i][j] = arr[i][j-1]
-                else: #lol need to check this not sure if its right.
-                    arr[i][j] = arr[i][j-1]
+                if (bottom_path[i][0] <= j - 1) and (bottom_path[i-1][0] <= j <= top_path[i-1][1]): #if left is in bounds and top is in bounds
+                    arr[i][j] = max(arr[i - 1][j], arr[i][j - 1])
+                elif (bottom_path[i][0] <= j - 1): #Left in bounds but not up or diagnoal
+                    arr[i][j] = arr[i][j - 1]
+                else:# (bottom_path[i-1][0] <= j <= top_path[i-1][1]): #up in bounds but not left
+                    arr[i][j] = arr[i - 1][j]
+                # else:
+                #     print j, bottom_path[i-1][0], top_path[i-1][1], bottom_path[i][0], top_path[i][1]
+                #     #print "NO VALID CASE?"
+        #     else:
+        #         #TODO: ADD BOUND CHECKING BASED ON LOWER AND UPPER PATHS
+        #         if (bottom_path[i-1][0] <= j <= top_path[i-1][1]):
+        #             if (bottom_path[i][0] <= j - 1): #not sure if we actually need to check this
+        #                 #print "here"
+        #                 arr[i][j] = max(arr[i - 1][j], arr[i][j - 1])
+        #             # else:
+        #             #      arr[i][j] = arr[i][j-1]
+        #         else: #lol need to check this not sure if its right.
+        #             arr[i][j] = arr[i][j-1]
     #print "AFTER: \n", arr
     global max_lcs_length
     max_lcs_length = max(max_lcs_length, arr[i][j])
@@ -67,12 +91,12 @@ def single_shortest_path(A, B, mid, top_path, bottom_path, top_row, bottom_row):
     m = len(A)
     path = [[0,n] for a in range(0, m+1)] #extra row
     while i > mid+1 and n > 0:
-        if A[i-1] == B[n-1]:
+        if A[i-1] == B[n-1] and (bottom_path[i-1][0] <= n - 1 <= top_path[i-1][1]):
             path[i-1][1] = n-1 #new row in upper bound case
             path[i][0] = n     #old row in lower bound case
             i = i - 1
             n = n - 1
-        elif arr[i-1][n] < arr[i][n-1]:
+        elif arr[i-1][n] < arr[i][n-1] and (bottom_path[i][0] <= n - 1) and (bottom_path[i-1][0] <= n <= top_path[i-1][1]):
             #BACKTRACE BY MOVING LEFT ON GRAPH. No updates necessary in upper bound case. 
                                                 #min for row updated in lower bound case
             path[i][0] = n-1 #current row in lower bound case
@@ -132,12 +156,13 @@ def main():
         sys.exit('Usage: `python LCS.py < input`')
 
     for l in sys.stdin:
+    # if 1 == 1:
         global arr 
-        arr = np.zeros((8,8), dtype=int)
+        arr = np.zeros((2048,2048), dtype=int)
         global max_lcs_length
         max_lcs_length = 0
         A, B = l.split()
-       # A, B = "C", "CDCC"
+        # A, B = "BBCAA", "ABABB"
         m = len(A)
         n = len(B)
         p = [[] for i in range(0, 2*m)]
@@ -151,7 +176,7 @@ def main():
         find_shortest_path(A, B, p, 0, m)
         #TODO: Take out break, print length of shortest path once we test.
         print max_lcs_length        #
-        break
+        #break
     return
 
 
